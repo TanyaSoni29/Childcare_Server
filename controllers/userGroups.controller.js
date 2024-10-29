b5f4ce286e9f00ec45dac5640c57fffcf8930d7c
TanyaSoni2912 <tanu139sona@gmail.com>
2024-10-29 17:10:55 +0530

    User group delete and create controller completed

--- controllers/userGroups.controller.js
+++ controllers/userGroups.controller.js
@@ -2,6 +2,27 @@
 
 const db = require('../db/db'); // Import the database connection
 
+const createUserGroups = async (req, res) => {
+	const { name, parent_id, privileges, sort_order, status } = req.body;
+	try {
+		const [result] = await db.query(
+			'INSERT INTO user_groups (name, parent_id, privileges, sort_order, status) VALUES (?, ?, ?, ?, ?)',
+			[name, parent_id, privileges, sort_order, status]
+		);
+		res.status(201).json({
+			id: result.insertId,
+			name,
+			parent_id,
+			privileges,
+			sort_order,
+			status,
+		});
+	} catch (err) {
+		console.error('Error inserting user group:', err);
+		res.status(500).send('Server error');
+	}
+};
+
 // Controller to get all user groups
 const getAllUserGroups = async (req, res) => {
 	try {
@@ -30,7 +51,25 @@ const getUserGroupById = async (req, res) => {
 	}
 };
 
+const deleteUserGroup = async (req, res) => {
+	const { id } = req.params;
+	try {
+		const [result] = await db.query('DELETE FROM user_groups WHERE id = ?', [
+			id,
+		]);
+		if (result.affectedRows === 0) {
+			return res.status(404).send('User group not found');
+		}
+		res.json({ message: 'User group deleted successfully' });
+	} catch (err) {
+		console.error('Error deleting user group:', err);
+		res.status(500).send('Server error');
+	}
+};
+
 module.exports = {
 	getAllUserGroups,
 	getUserGroupById,
+	createUserGroups,
+	deleteUserGroup,
 };
--- routes/usersGroups.route.js
+++ routes/usersGroups.route.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const userGroupController = require('../controllers/userGroups.controller');
 
 // Route to get all user groups
+router.post('/', userGroupController.createUserGroups);
 router.get('/', userGroupController.getAllUserGroups);
 
 // Route to get a single user group by ID
 router.get('/:id', userGroupController.getUserGroupById);
+router.delete('/:id', userGroupController.deleteUserGroup);
 
 module.exports = router;
